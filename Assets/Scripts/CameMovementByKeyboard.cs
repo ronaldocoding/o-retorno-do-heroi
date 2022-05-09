312661cf3a58aab3f5aa495d807ed09a5a8e2ddc
ronaldocosta <ronaldocosta.developer@gmail.com>
2022-05-08 21:07:28 -0400

    Fix minor bugs and improvements at level 1 scenario

---
+++ Assets/Scripts/CameMovementByKeyboard.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameMovementByKeyboard : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.RightArrow))
+     {
+         transform.Translate(new Vector3(speed * Time.deltaTime,0,0));
+     }
+     if(Input.GetKey(KeyCode.LeftArrow))
+     {
+         transform.Translate(new Vector3(-speed * Time.deltaTime,0,0));
+     }
+     if(Input.GetKey(KeyCode.DownArrow))
+     {
+         transform.Translate(new Vector3(0,-speed * Time.deltaTime,0));
+     }
+     if(Input.GetKey(KeyCode.UpArrow))
+     {
+         transform.Translate(new Vector3(0,speed * Time.deltaTime,0));
+     }
+    }
+}
