34cbe3426ae9c44f8ff79d783abc6f0ae4da5b88
ronaldocosta <ronaldocosta.developer@gmail.com>
2022-05-14 01:53:18 -0400

    Add basic hero-final-boss interaction

---
+++ Assets/Scripts/CagliostroScripts/CagliostroHotZoneCheck.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CagliostroHotZoneCheck : MonoBehaviour
+{
+
+    private CagliostroFollow enemyParent;
+    private bool inRange;
+    private Animator anim;
+
+    private void Awake()
+    {
+        enemyParent = GetComponentInParent<CagliostroFollow>();
+        anim = GetComponentInParent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inRange && !inRange && !anim.GetCurrentAnimatorStateInfo(0).IsName(enemyParent.AttackAnimationName))
+        {
+            enemyParent.Flip();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag=="Player")
+        {
+            inRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag=="Player")
+        {
+            inRange = false;
+
+            gameObject.SetActive(false);
+            enemyParent.triggerArea.SetActive(true);
+            enemyParent.inRange = false;
+
+            enemyParent.SelectTarget();
+        }
+    }
+}
