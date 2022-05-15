2b3c5afda70dc316b7366e0e4cc2125dfb6eb33f
Thiago Marcelo Uchôa Marques <thiago.uchoa18@gmail.com>
2022-05-15 00:41:19 -0400

    fixing some final issues

--- Assets/BossHurtScript.cs
+++ Assets/BossHurtScript.cs
@@ -4,12 +4,9 @@ using UnityEngine;
 
 public class BossHurtScript : StateMachineBehaviour
 {
-    private MiniBoss boss;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        boss = animator.GetComponent<MiniBoss>();
-        boss.stopAttack();
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
